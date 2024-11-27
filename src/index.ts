194ac95568b158ab4d38debf7f0a3e8501ea2527
KuldeepJha5176 <ranacjha@gmail.com>
2024-11-27 22:41:50 +0530

    send message to client

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,21 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-
 let userCount = 0;
+let allSockets: WebSocket[] = []
 
 wss.on("connection", (socket) => {
-    userCount = userCount + 1;
-    console.log("User connected #" + userCount)
-    
-    socket.on("message", (message) => {
-    console.log("message received " + message.toString())
-    setTimeout(() => {
-        socket.send(message.toString() + "message received from the server")
-    },1000)
-    
-    })
-    })
+  allSockets.push(socket);
+  userCount = userCount + 1;
+  console.log("User connected #" + userCount);
 
+  socket.on("message", (message) => {
+    console.log("message received " + message.toString());
+    setTimeout(() => {
+      for (let i = 0; i < allSockets.length; i++) {
+        const s = allSockets[i];
+        s.send(message.toString() + "message received from the server");
+      }
+    }, 1000);
+  });
+});
