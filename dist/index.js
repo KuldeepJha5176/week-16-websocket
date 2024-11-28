eab8a8b42cedca14775d041cdedf534bf94bb343
KuldeepJha5176 <ranacjha@gmail.com>
2024-11-28 13:30:04 +0530

    completed the backend of chat application

--- dist/index.js
+++ dist/index.js
@@ -2,14 +2,29 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
-let userCount = 0;
+const allSockets = new Map();
 wss.on("connection", (socket) => {
-    userCount = userCount + 1;
-    console.log("User connected #" + userCount);
     socket.on("message", (message) => {
-        console.log("message received " + message.toString());
-        setTimeout(() => {
-            socket.send(message.toString() + "message received from the server");
-        }, 1000);
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type == "join") {
+            const socketId = parsedMessage.payload.userId || socket.protocol || generateUniqueId();
+            allSockets.set(socketId, { socket, room: parsedMessage.payload.roomId });
+        }
+        if (parsedMessage.type == "chat") {
+            // const currentUserRoom = Array.from(allSockets.values()).find((x) => x.socket = socket)?.room
+            let currentUserRoom = null;
+            for (const [key, value] of allSockets) {
+                if ((value.socket === socket)) {
+                    currentUserRoom = value.room;
+                } // iteration in maps in typescript
+            }
+            for (const [key, value] of allSockets) {
+                if (value.room == currentUserRoom)
+                    value.socket.send(parsedMessage.payload.message);
+            }
+        }
     });
+    function generateUniqueId() {
+        return Math.random().toString(36).substring(2, 15);
+    }
 });
--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,30 @@ interface User {
   room: string;
 }
 
-
 const allSockets = new Map<string, User>();
 wss.on("connection", (socket) => {
-  
-    
   socket.on("message", (message) => {
-   const parsedMessage = JSON.parse(message);
-   if(parsedMessage.type == "join" ){
-    const socketId = parsedMessage.payload.userId || socket.protocol || generateUniqueId();
-    allSockets.set(socketId,
-      {socket,
-      room: parsedMessage.payload.roomId}
-    )
-   }
+    const parsedMessage = JSON.parse(message as unknown as string);
+    if (parsedMessage.type == "join") {
+      const socketId =
+        parsedMessage.payload.userId || socket.protocol || generateUniqueId();
+      allSockets.set(socketId, { socket, room: parsedMessage.payload.roomId });
+    }
+
+    if (parsedMessage.type == "chat") {
+      // const currentUserRoom = Array.from(allSockets.values()).find((x) => x.socket = socket)?.room
+      let currentUserRoom = null;
+      for (const [key, value] of allSockets) {
+        if ((value.socket === socket)) {
+          currentUserRoom = value.room;
+        } // iteration in maps in typescript
+      }
+      for(const [key,value] of allSockets) {
+        if(value.room == currentUserRoom)
+          value.socket.send(parsedMessage.payload.message)
+      }
+    }
+  
   });
   function generateUniqueId(): string {
     return Math.random().toString(36).substring(2, 15);
